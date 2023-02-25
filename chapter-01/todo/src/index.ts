4234297f2e500e1254de1a86486aca5e3ac1250f
fakynno <fakynno@gmail.com>
2023-02-25 13:05:13 -0300

    Alteracao na exibicao das informacoes

--- chapter-01/todo/src/index.ts
+++ chapter-01/todo/src/index.ts
@@ -4,7 +4,7 @@ import { TodoCollection } from "./todoCollection";
 let todos: TodoItem[] = [
     new TodoItem(1, "Estudar Typescript", true),
     new TodoItem(2, "Estudar Java"),
-    new TodoItem(3, "Tocar Violão"),
+    new TodoItem(3, "Tocar Violão", true),
     new TodoItem(4, "Aprender Inglês")
 ];
 
@@ -13,6 +13,4 @@ let collection: TodoCollection = new TodoCollection("Fabio", todos);
 console.clear();
 console.log(`Lista TODO de ${collection.userName}`);
 
-let newId: number = collection.addTodo("Fazer academia");
-let todoItem: TodoItem = collection.getTodoById(newId);
-todoItem.printDetails();
+collection.getTodoItems(true).forEach(item => item.printDetails());
