73db85584e36a180470a6f7671556149977d48fc
fakynno <fakynno@gmail.com>
2023-02-25 10:56:12 -0300

    Add metodo para pegar o Id do TODO

--- chapter-01/todo/src/todoCollection.ts
+++ chapter-01/todo/src/todoCollection.ts
@@ -4,5 +4,9 @@ export class TodoCollection {
     private nextId: number = 1;
 
     constructor(public userName: string,
-                public todoItems: TodoItem[] = []) { }
+        public todoItems: TodoItem[] = []) { }
+
+    getTodoById(id: number): TodoItem {
+        return this.todoItems.find(item => item.id === id);
+    }
 }
