fe10f4828e5df7cd057c888a3fbe0e93e9bab3e9
fakynno <fakynno@gmail.com>
2023-02-25 12:24:14 -0300

    Alteracao nos tipos de dados para evitar erros

--- chapter-01/todo/src/index.ts
+++ chapter-01/todo/src/index.ts
@@ -1,20 +1,18 @@
 import { TodoItem } from "./todoItem";
 import { TodoCollection } from "./todoCollection";
 
-let todos = [
+let todos: TodoItem[] = [
     new TodoItem(1, "Estudar Typescript", true),
     new TodoItem(2, "Estudar Java"),
     new TodoItem(3, "Tocar Violão"),
     new TodoItem(4, "Aprender Inglês")
 ];
 
-let collection = new TodoCollection("Fabio", todos);
+let collection: TodoCollection = new TodoCollection("Fabio", todos);
 
 console.clear();
 console.log(`Lista TODO de ${collection.userName}`);
 
-let newId = collection.addTodo("Fazer academia");
-let todoItem = collection.getTodoById(newId);
+let newId: number = collection.addTodo("Fazer academia");
+let todoItem: TodoItem = collection.getTodoById(newId);
 todoItem.printDetails();
-
-collection.addTodo(todoItem);
