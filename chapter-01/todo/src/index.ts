34420e7e7580ac3ccca1945b619d2bff038b75cf
fakynno <fakynno@gmail.com>
2023-02-25 11:08:08 -0300

    Mock de TODOs para testar o codigo

--- chapter-01/todo/src/index.ts
+++ chapter-01/todo/src/index.ts
@@ -1,2 +1,12 @@
+import { TodoItem } from "./todoItem";
+import { TodoCollection } from "./todoCollection";
+
+let todos = [
+    new TodoItem(1, "Estudar Typescript", true),
+    new TodoItem(2, "Estudar Java"),
+    new TodoItem(3, "Tocar Violão"),
+    new TodoItem(4, "Aprender Inglês")
+];
+
 console.clear();
 console.log("Lista TODO de Fabio");
