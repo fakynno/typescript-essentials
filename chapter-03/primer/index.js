bd6113dd51a51fec09e711326904545847ddfa58
fakynno <fakynno@gmail.com>
2023-03-09 19:24:42 -0300

    Exemplos de intro ao javascript

---
+++ chapter-03/primer/index.js
@@ -0,0 +1,30 @@
+let precoChapeu = 100;
+console.log(`Preço do Chapéu: R$ ${precoChapeu},00`);
+let precoBotas = "100";
+console.log(`Preço das Botas: R$ ${precoBotas},00`);
+console.log('*********************');
+
+let valorTaxa; // não foi definido valor da taxa
+console.log(`Valor da taxa antes: ${ valorTaxa || 10}%`);
+valorTaxa = 0; // valor zero para a taxa
+console.log(`Valor da taxa depois: ${ valorTaxa || 10 }%`);
+
+if (precoBotas == precoChapeu) {
+    console.log('Os preços são iguais');
+} else {
+    console.log('Os Preços são diferentes');
+}
+
+console.log('*********************');
+let precoTotal = Number(precoBotas) + Number(precoChapeu);
+console.log(`Preço total: ${precoTotal}`);
+
+let nome = "Fabio";
+console.log(`Tipo: ${typeof nome}`);
+nome = 150;
+console.log(`Tipo: ${typeof nome}`);
+
+let primeiraCidade;
+console.log(`Variável primeiraCidade é do tipo: ${ typeof primeiraCidade }`);
+let segundaCidade = primeiraCidade || "São Paulo";
+console.log(`Cidade: ${ segundaCidade }`);
