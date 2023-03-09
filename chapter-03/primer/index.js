f139a7a03ea32ed87ea33d1185ba9164ce5df237
fakynno <fakynno@gmail.com>
2023-03-09 19:44:20 -0300

    Ajuste no parametro da funcao

--- chapter-03/primer/index.js
+++ chapter-03/primer/index.js
@@ -7,23 +7,19 @@ console.log('*********************');
 console.log('*********************');
 let precoTotal = somaPrecos(precoBotas, precoChapeu);
 // let precoTotal = Number(precoBotas) + Number(precoChapeu);
-console.log(`Preço total: ${ precoTotal } ${ typeof precoTotal }`);
+console.log(`Preço total: ${ precoTotal }; variável é do tipo:  ${ typeof precoTotal }`);
 
 precoTotal = somaPrecos(100, 200, 300);
-console.log(`Preço total: ${ precoTotal } ${ typeof precoTotal }`);
+console.log(`Preço total: ${ precoTotal }; variável é do tipo:  ${ typeof precoTotal }`);
 
 precoTotal = somaPrecos(100, 200);
-console.log(`Preço total: ${ precoTotal } ${ typeof precoTotal}`);
+console.log(`Preço total: ${ precoTotal }; variável é do tipo:  ${ typeof precoTotal}`);
 
 
 let valorTaxa; // não foi definido valor da taxa
 console.log(`Valor da taxa antes: ${ valorTaxa ?? 10}%`);
 valorTaxa = 0; // valor zero para a taxa
 console.log(`Valor da taxa depois: ${ valorTaxa ?? 10 }%`);
-
-function somaPrecos(primeiro, segundo, terceiro) {
-    return primeiro + segundo + terceiro;
-}
 
 if (precoBotas == precoChapeu) {
     console.log('Os preços são iguais');
@@ -41,3 +37,8 @@ let primeiraCidade;
 console.log(`Variável primeiraCidade é do tipo: ${ typeof primeiraCidade }`);
 let segundaCidade = primeiraCidade || "São Paulo";
 console.log(`Cidade: ${ segundaCidade }`);
+
+
+function somaPrecos(primeiro, segundo, terceiro = 0) {
+    return primeiro + segundo + terceiro;
+}
